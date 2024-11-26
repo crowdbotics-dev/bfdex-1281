f0401cd89151aa67ea480433182e983313d812af
Shubham qatesthub <shubham.malik@crowdbotics.com>
2024-11-26 12:06:13 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/bfdexAPI/api.js
+++ store/bfdexAPI/api.js
@@ -1,68 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 const bfdexAPI = axios.create({
   baseURL: "https://bfdex-1281.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
-  return bfdexAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return bfdexAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return bfdexAPI.post(`/api/v1/login/`, payload)
+  return bfdexAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return bfdexAPI.post(`/api/v1/signup/`, payload)
+  return bfdexAPI.post(`/api/v1/signup/`, payload);
 }
+
 function api_v1_yhbchd_list(payload) {
-  return bfdexAPI.get(`/api/v1/yhbchd/`)
+  return bfdexAPI.get(`/api/v1/yhbchd/`);
 }
+
 function api_v1_yhbchd_create(payload) {
-  return bfdexAPI.post(`/api/v1/yhbchd/`, payload)
+  return bfdexAPI.post(`/api/v1/yhbchd/`, payload);
 }
+
 function api_v1_yhbchd_retrieve(payload) {
-  return bfdexAPI.get(`/api/v1/yhbchd/${payload.id}/`)
+  return bfdexAPI.get(`/api/v1/yhbchd/${payload.id}/`);
 }
+
 function api_v1_yhbchd_update(payload) {
-  return bfdexAPI.put(`/api/v1/yhbchd/${payload.id}/`, payload)
+  return bfdexAPI.put(`/api/v1/yhbchd/${payload.id}/`, payload);
 }
+
 function api_v1_yhbchd_partial_update(payload) {
-  return bfdexAPI.patch(`/api/v1/yhbchd/${payload.id}/`, payload)
+  return bfdexAPI.patch(`/api/v1/yhbchd/${payload.id}/`, payload);
 }
+
 function api_v1_yhbchd_destroy(payload) {
-  return bfdexAPI.delete(`/api/v1/yhbchd/${payload.id}/`)
+  return bfdexAPI.delete(`/api/v1/yhbchd/${payload.id}/`);
 }
+
 function rest_auth_login_create(payload) {
-  return bfdexAPI.post(`/rest-auth/login/`, payload)
+  return bfdexAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_create(payload) {
-  return bfdexAPI.post(`/rest-auth/logout/`)
+  return bfdexAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return bfdexAPI.post(`/rest-auth/password/change/`, payload)
+  return bfdexAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return bfdexAPI.post(`/rest-auth/password/reset/`, payload)
+  return bfdexAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return bfdexAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return bfdexAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return bfdexAPI.post(`/rest-auth/registration/`, payload)
+  return bfdexAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_resend_email_create(payload) {
-  return bfdexAPI.post(`/rest-auth/registration/resend-email/`, payload)
+  return bfdexAPI.post(`/rest-auth/registration/resend-email/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return bfdexAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return bfdexAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return bfdexAPI.get(`/rest-auth/user/`)
+  return bfdexAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return bfdexAPI.put(`/rest-auth/user/`, payload)
+  return bfdexAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return bfdexAPI.patch(`/rest-auth/user/`, payload)
+  return bfdexAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -84,4 +112,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
--- store/bfdexAPI/yhbchds.slice.js
+++ store/bfdexAPI/yhbchds.slice.js
@@ -1,167 +1,128 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const api_v1_yhbchd_list = createAsyncThunk(
-  "yhbchds/api_v1_yhbchd_list",
-  async payload => {
-    const response = await apiService.api_v1_yhbchd_list(payload)
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const api_v1_yhbchd_list = createAsyncThunk("yhbchds/api_v1_yhbchd_list", async payload => {
+  const response = await apiService.api_v1_yhbchd_list(payload);
+  return response.data;
+});
+export const api_v1_yhbchd_create = createAsyncThunk("yhbchds/api_v1_yhbchd_create", async payload => {
+  const response = await apiService.api_v1_yhbchd_create(payload);
+  return response.data;
+});
+export const api_v1_yhbchd_retrieve = createAsyncThunk("yhbchds/api_v1_yhbchd_retrieve", async payload => {
+  const response = await apiService.api_v1_yhbchd_retrieve(payload);
+  return response.data;
+});
+export const api_v1_yhbchd_update = createAsyncThunk("yhbchds/api_v1_yhbchd_update", async payload => {
+  const response = await apiService.api_v1_yhbchd_update(payload);
+  return response.data;
+});
+export const api_v1_yhbchd_partial_update = createAsyncThunk("yhbchds/api_v1_yhbchd_partial_update", async payload => {
+  const response = await apiService.api_v1_yhbchd_partial_update(payload);
+  return response.data;
+});
+export const api_v1_yhbchd_destroy = createAsyncThunk("yhbchds/api_v1_yhbchd_destroy", async payload => {
+  const response = await apiService.api_v1_yhbchd_destroy(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-export const api_v1_yhbchd_create = createAsyncThunk(
-  "yhbchds/api_v1_yhbchd_create",
-  async payload => {
-    const response = await apiService.api_v1_yhbchd_create(payload)
-    return response.data
-  }
-)
-export const api_v1_yhbchd_retrieve = createAsyncThunk(
-  "yhbchds/api_v1_yhbchd_retrieve",
-  async payload => {
-    const response = await apiService.api_v1_yhbchd_retrieve(payload)
-    return response.data
-  }
-)
-export const api_v1_yhbchd_update = createAsyncThunk(
-  "yhbchds/api_v1_yhbchd_update",
-  async payload => {
-    const response = await apiService.api_v1_yhbchd_update(payload)
-    return response.data
-  }
-)
-export const api_v1_yhbchd_partial_update = createAsyncThunk(
-  "yhbchds/api_v1_yhbchd_partial_update",
-  async payload => {
-    const response = await apiService.api_v1_yhbchd_partial_update(payload)
-    return response.data
-  }
-)
-export const api_v1_yhbchd_destroy = createAsyncThunk(
-  "yhbchds/api_v1_yhbchd_destroy",
-  async payload => {
-    const response = await apiService.api_v1_yhbchd_destroy(payload)
-    return response.data
-  }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const yhbchdsSlice = createSlice({
   name: "yhbchds",
   initialState,
   reducers: {},
   extraReducers: builder => {
-    builder
-      .addCase(api_v1_yhbchd_list.pending, (state, action) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending"
-        }
-      })
-      .addCase(api_v1_yhbchd_list.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.entities = action.payload
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_list.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_create.pending, (state, action) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending"
-        }
-      })
-      .addCase(api_v1_yhbchd_create.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.entities.push(action.payload)
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_create.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_retrieve.pending, (state, action) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending"
-        }
-      })
-      .addCase(api_v1_yhbchd_retrieve.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.entities = [
-            ...state.entities.filter(record => record.id !== action.payload.id),
-            action.payload
-          ]
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_retrieve.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_update.pending, (state, action) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending"
-        }
-      })
-      .addCase(api_v1_yhbchd_update.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.entities = state.entities.map(record =>
-            record.id === action.payload.id ? action.payload : record
-          )
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_update.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_partial_update.pending, (state, action) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending"
-        }
-      })
-      .addCase(api_v1_yhbchd_partial_update.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.entities = state.entities.map(record =>
-            record.id === action.payload.id ? action.payload : record
-          )
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_partial_update.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_destroy.pending, (state, action) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending"
-        }
-      })
-      .addCase(api_v1_yhbchd_destroy.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.entities = state.entities.filter(
-            record => record.id !== action.meta.arg?.id
-          )
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(api_v1_yhbchd_destroy.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error
-          state.api.loading = "idle"
-        }
-      })
+    builder.addCase(api_v1_yhbchd_list.pending, (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending";
+      }
+    }).addCase(api_v1_yhbchd_list.fulfilled, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = action.payload;
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_list.rejected, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error;
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_create.pending, (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending";
+      }
+    }).addCase(api_v1_yhbchd_create.fulfilled, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities.push(action.payload);
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_create.rejected, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error;
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_retrieve.pending, (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending";
+      }
+    }).addCase(api_v1_yhbchd_retrieve.fulfilled, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [...state.entities.filter(record => record.id !== action.payload.id), action.payload];
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_retrieve.rejected, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error;
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_update.pending, (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending";
+      }
+    }).addCase(api_v1_yhbchd_update.fulfilled, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.map(record => record.id === action.payload.id ? action.payload : record);
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_update.rejected, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error;
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_partial_update.pending, (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending";
+      }
+    }).addCase(api_v1_yhbchd_partial_update.fulfilled, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.map(record => record.id === action.payload.id ? action.payload : record);
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_partial_update.rejected, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error;
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_destroy.pending, (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending";
+      }
+    }).addCase(api_v1_yhbchd_destroy.fulfilled, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.filter(record => record.id !== action.meta.arg?.id);
+        state.api.loading = "idle";
+      }
+    }).addCase(api_v1_yhbchd_destroy.rejected, (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error;
+        state.api.loading = "idle";
+      }
+    });
   }
-})
+});
 export default {
   api_v1_yhbchd_list,
   api_v1_yhbchd_create,
@@ -170,4 +131,4 @@ export default {
   api_v1_yhbchd_partial_update,
   api_v1_yhbchd_destroy,
   slice: yhbchdsSlice
-}
+};
